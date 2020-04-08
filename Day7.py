128b72a61b44875b43754110dc5193c784d6af92
Kushal997-das <61356005+Kushal997-das@users.noreply.github.com>
2020-04-08 13:09:16 +0530

    Day 7: Arrays

---
+++ Day7.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr =list( map(int, input().rstrip().split()))
+    print(" ".join(map(str,arr[::-1])))
