18be213b4e8992c593410a0573def0a83d9dc523
Kushal997-das <61356005+Kushal997-das@users.noreply.github.com>
2020-04-10 11:55:10 +0530

    Day 8: Dictionaries and Maps

---
+++ Day8.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+
+n = int(sys.stdin.readline().strip())
+phoneBook = dict()
+
+for i in range(0, n):
+    entry = sys.stdin.readline().strip().split(' ')
+    phoneBook[entry[0]] = entry[1]
+
+query = sys.stdin.readline().strip()
+while query:
+    phoneNumber = phoneBook.get(query)
+    if phoneNumber:
+        print(query + '=' + phoneNumber)
+    else:
+        print('Not found')
+    query = sys.stdin.readline().strip()
+
